791a4f105ad111e3449e6307610c8033700992e7
Denis Tsap <ts.dios.v@gmail.com>
2020-12-03 20:29:46 +0300

    Разделяет js-код на блоки: для mobile-tablet и  desktop

--- source/js/app.js
+++ source/js/app.js
@@ -11,51 +11,66 @@ const burgerRect = auxiliaryLayout.querySelectorAll( '.general-header__burger-re
 const crossSvg = primaryLayout.querySelector( '.general-header__cross-svg' );
 const lining = document.querySelector( '.page__lining' );
 
-const switchLayout = function() {
-  generalHeader.style.position = 'fixed';
-  lining.classList.remove( 'invisible' );
-  primaryLayout.classList.add( 'invisible' );
-  // auxiliaryLayout.classList.remove( 'invisible' );
-  auxiliaryLayout.style.display = 'flex';
+const currentMode = ( document.documentElement.clientWidth < 1440 ) ? headerMobileTablet : headerDesktop;
+currentMode();
 
-  if ( generalHeader.classList.contains( 'general-header--form' ) ) {
-    auxiliaryLayout.classList.add( 'general-header__auxiliary-layout--form' );
+function headerMobileTablet() {
+  const switchLayout = function() {
+    generalHeader.style.position = 'fixed';
+    lining.classList.remove( 'invisible' );
+    primaryLayout.classList.add( 'invisible' );
+    // auxiliaryLayout.classList.remove( 'invisible' );
+    auxiliaryLayout.style.display = 'flex';
+
+    if ( generalHeader.classList.contains( 'general-header--form' ) ) {
+      auxiliaryLayout.classList.add( 'general-header__auxiliary-layout--form' );
+    }
+
+    if ( generalHeader.classList.contains( 'general-header--catalog' ) ) {
+      auxiliaryLayout.classList.add( 'general-header__auxiliary-layout--catalog' );
+    }
+
+    cross.classList.remove( 'invisible' );
+    crossSvg.classList.remove( 'invisible' );
   }
 
-  if ( generalHeader.classList.contains( 'general-header--catalog' ) ) {
-    auxiliaryLayout.classList.add( 'general-header__auxiliary-layout--catalog' );
-  }
+  switchLayout();
 
-  cross.classList.remove( 'invisible' );
-  crossSvg.classList.remove( 'invisible' );
+  burger.addEventListener( 'click', function() {
+    primaryLayout.classList.remove( 'invisible' );
+    primaryLayout.classList.add( 'general-header__primary-layout--positioned' );
+  } );
+
+  cross.addEventListener( 'click', function() {
+    primaryLayout.classList.add( 'invisible' );
+    primaryLayout.classList.remove( 'general-header__primary-layout--positioned' );
+  } );
+
+  window.onscroll = function() {
+    if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+      auxiliaryLayout.classList.add( 'general-header__auxiliary-layout--scroll' );
+      logoPrime[ 0 ].classList.remove( 'invisible' );
+      logoAux[ 0 ].classList.add( 'invisible' );
+      for ( let i = 0; i < burgerRect.length; i++ ) {
+        burgerRect[ i ].classList.add( 'general-header__burger-rect--scroll' );
+      }
+    } else {
+      auxiliaryLayout.classList.remove( 'general-header__auxiliary-layout--scroll' );
+      logoPrime[ 0 ].classList.add( 'invisible' );
+      logoAux[ 0 ].classList.remove( 'invisible' );
+      for ( let i = 0; i < burgerRect.length; i++ ) {
+        burgerRect[ i ].classList.remove( 'general-header__burger-rect--scroll' );
+      }
+    }
+  }
 }
 
-switchLayout();
-
-burger.addEventListener( 'click', function() {
-  primaryLayout.classList.remove( 'invisible' );
-  primaryLayout.classList.add( 'general-header__primary-layout--positioned' );
-} );
-
-cross.addEventListener( 'click', function() {
-  primaryLayout.classList.add( 'invisible' );
-  primaryLayout.classList.remove( 'general-header__primary-layout--positioned' );
-} );
-
-window.onscroll = function() {
-  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
-    auxiliaryLayout.classList.add( 'general-header__auxiliary-layout--scroll' );
-    logoPrime[ 0 ].classList.remove( 'invisible' );
-    logoAux[ 0 ].classList.add( 'invisible' );
-    for ( let i = 0; i < burgerRect.length; i++ ) {
-      burgerRect[ i ].classList.add( 'general-header__burger-rect--scroll' );
-    }
-  } else {
-    auxiliaryLayout.classList.remove( 'general-header__auxiliary-layout--scroll' );
-    logoPrime[ 0 ].classList.add( 'invisible' );
-    logoAux[ 0 ].classList.remove( 'invisible' );
-    for ( let i = 0; i < burgerRect.length; i++ ) {
-      burgerRect[ i ].classList.remove( 'general-header__burger-rect--scroll' );
+function headerDesktop() {
+  window.onscroll = function() {
+    if ( window.pageYOffset > 830 ) {
+      primaryLayout.classList.add( 'general-header__primary-layout--scrolled' );
+    } else {
+      primaryLayout.classList.remove( 'general-header__primary-layout--scrolled' );
     }
   }
-};
+}
