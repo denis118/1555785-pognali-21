da62e22b482bf00d37172fecfb9e81b38f4c4983
Denis Tsap <ts.dios.v@gmail.com>
2021-01-10 23:36:59 +0300

    Пишет код для перехода в JS режим идля скроллинга

--- source/js/app.js
+++ source/js/app.js
@@ -1,33 +1,28 @@
-'use strict';
+"use strict";
 
 let screenWidth = document.documentElement.clientWidth;
-const pageBody = document.querySelector('.page__body');
-const generalHeader = document.querySelector('.general-header');
-const wrapper = generalHeader.querySelector('.general-header__wrapper');
-const lowerContainer = generalHeader.querySelector('.general-header__container--lower');
-const logoLink = generalHeader.querySelector('.general-header__logo');
-const logoPrime = generalHeader.querySelector('.logo__image--primary');
-const logoAux = generalHeader.querySelector('.logo__image--auxiliary');
-const toggle = generalHeader.querySelector('.general-header__toggle');
-const burger = toggle.querySelector('.general-header__burger-svg');
-const burgerRect = burger.querySelectorAll('.general-header__burger-rect');
-const crosses = toggle.querySelectorAll('.general-header__cross-svg');
-const navigation = generalHeader.querySelector('.general-header__site-navigation');
-const authorization = generalHeader.querySelector('.general-header__authorization');
-const authorizationText = authorization.querySelector('.authorization__text');
-const contactsGroup = generalHeader.querySelector('.general-header__contacts-group');
-const initialContactIcons = contactsGroup.querySelectorAll('.contacts-group__svg--initial');
-const inlineContactIcons = contactsGroup.querySelectorAll('.contacts-group__svg--inline');
-const contactsContent = contactsGroup.querySelectorAll('.contacts-group__span');
-const socials = generalHeader.querySelector('.general-header__socials');
-const main = document.querySelector('.main');
+const pageBody = document.querySelector(".page__body");
+const generalHeader = document.querySelector(".general-header");
+const container = generalHeader.querySelector(".general-header__container");
+const logoLink = generalHeader.querySelector(".general-header__logo");
+const logoPrime = generalHeader.querySelector(".logo__image--primary");
+const logoAux = generalHeader.querySelector(".logo__image--auxiliary");
+const toggle = generalHeader.querySelector(".general-header__toggle");
+const burger = toggle.querySelector(".general-header__burger-svg");
+const burgerRect = burger.querySelectorAll(".general-header__burger-rect");
+const crosses = toggle.querySelectorAll(".general-header__cross-svg");
+const navigation = generalHeader.querySelector(".general-header__site-navigation");
+const authorization = generalHeader.querySelector(".general-header__authorization");
+const contactsGroup = generalHeader.querySelector(".general-header__contacts-group");
+const socials = generalHeader.querySelector(".general-header__socials");
+const main = document.querySelector(".main");
 const email = document.querySelector("#email");
 const errorMessage = document.querySelector("#error-message");
 const submitButton = document.querySelector("#submit");
 
 jsMode();
 // window.addEventListener('resize', jsMode);
-// window.addEventListener('scroll', scrollMode);
+window.addEventListener('scroll', scrollMode);
 // burger.addEventListener('click', openSiteMenu);
 // crosses.forEach(function (item) {
 //   item.addEventListener('click', closeSiteMenu);
@@ -58,28 +53,119 @@ jsMode();
 // }
 
 function jsMode() {
-  const elements = [
-    pageBody,
-    generalHeader,
-    wrapper,
-    logoPrime,
-    logoAux,
-    toggle,
-    burger,
-    crosses,
-    navigation,
-    authorization,
-    authorizationText,
-    contactsGroup,
-    initialContactIcons,
-    inlineContactIcons,
-    contactsContent,
-    socials
-  ];
+  // const elements = [
+  //   pageBody,
+  //   generalHeader,
+  //   container,
+  //   logoLink,
+  //   logoPrime,
+  //   logoAux,
+  //   toggle,
+  //   burger,
+  //   burgerRect,
+  //   crosses,
+  //   navigation,
+  //   authorization,
+  //   contactsGroup,
+  //   socials,
+  //   main
+  // ];
 
-  console.log(elements);
+  // console.log(elements);
+
+  generalHeader.classList.add("general-header--js");
+  main.classList.add("main--js");
+
+  if (screenWidth < 1440) {
+    if (generalHeader.classList.contains("general-header--index")) {
+      generalHeader.classList.add("general-header--index-js");
+    }
+
+    logoAux.classList.remove("hidden-before-desktop");
+    toggle.classList.remove("hidden-entity");
+
+    if (screenWidth < 768) {
+      authorization.classList.add("hidden-entity");
+    }
+
+    if (screenWidth > 767) {
+      if (authorization.classList.contains("hidden-entity")) {
+        authorization.classList.remove("hidden-entity");
+      }
+      authorization.classList.remove("authorization--modified");
+    }
+
+    const hiddenElements = [
+      logoPrime,
+      navigation,
+      contactsGroup,
+      socials
+    ];
+
+    hiddenElements.forEach((item) => {
+      item.classList.add("hidden-entity");
+    });
+  }
 }
 
+function scrollMode() {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    generalHeader.classList.add("general-header--scrolled");
+
+    if (!logoAux.classList.contains("hidden-before-desktop")) {
+      logoAux.classList.add("hidden-before-desktop");
+    }
+
+    if (logoPrime.classList.contains("hidden-entity")) {
+      logoPrime.classList.remove("hidden-entity");
+    }
+
+    if (screenWidth < 1440) {
+      burgerRect.forEach((item) => {
+        item.classList.add("general-header__burger-rect--scrolled");
+      });
+    }
+
+    if (screenWidth > 1439) {
+      logoAux.classList.add("hidden-entity");
+
+      if (logoPrime.classList.contains("hidden-on-desktop")) {
+        logoPrime.classList.remove("hidden-on-desktop");
+      }
+
+      navigation.classList.add("general-header__site-navigation--scrolled");
+      contactsGroup.classList.add("general-header__contacts-group--scrolled");
+    }
+
+  } else {
+    generalHeader.classList.remove("general-header--scrolled");
+
+    if (logoAux.classList.contains("hidden-before-desktop")) {
+      logoAux.classList.remove("hidden-before-desktop");
+    }
+
+    if (!logoPrime.classList.contains("hidden-entity")) {
+      logoPrime.classList.add("hidden-entity");
+    }
+
+    if (screenWidth < 1440) {
+      burgerRect.forEach((item) => {
+        item.classList.remove("general-header__burger-rect--scrolled");
+      });
+    }
+
+    if (screenWidth > 1439) {
+      logoAux.classList.remove("hidden-entity");
+
+      if (!logoPrime.classList.contains("hidden-on-desktop")) {
+        logoPrime.classList.add("hidden-on-desktop");
+      }
+
+      navigation.classList.remove("general-header__site-navigation--scrolled");
+      contactsGroup.classList.remove("general-header__contacts-group--scrolled");
+    }
+  }
+}
 
 
 
@@ -127,11 +213,11 @@ function jsMode() {
 //   console.log(elements);
 
 //   if (screenWidth < 1440) {
-//     generalHeader.classList.add('general-header--js');
+    // generalHeader.classList.add('general-header--js');
 
-//     if (generalHeader.classList.contains('general-header--index')) {
-//       generalHeader.classList.add('general-header--index-js');
-//     }
+    // if (generalHeader.classList.contains('general-header--index')) {
+    //   generalHeader.classList.add('general-header--index-js');
+    // }
 
 //     logoLink.classList.add('general-header__logo--js');
 //     logoAux.classList.remove('hidden-before-desktop');
@@ -230,22 +316,22 @@ function jsMode() {
 // }
 
 // function scrollMode() {
-//   if (screenWidth < 1440) {
-//     if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-//       generalHeader.classList.add('generalHeader--scrolled');
-//       wrapper.classList.add('general-header__wrapper--scrolled');
+  // if (screenWidth < 1440) {
+  //   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+  //     generalHeader.classList.add('generalHeader--scrolled');
+  //     wrapper.classList.add('general-header__wrapper--scrolled');
 
-//       if (!logoAux.classList.contains('hidden-before-desktop')) {
-//         logoAux.classList.add('hidden-before-desktop');
-//       }
+      // if (!logoAux.classList.contains('hidden-before-desktop')) {
+      //   logoAux.classList.add('hidden-before-desktop');
+      // }
 
-//       if (logoPrime.classList.contains('hidden-entity')) {
-//         logoPrime.classList.remove('hidden-entity');
-//       }
+      // if (logoPrime.classList.contains('hidden-entity')) {
+      //   logoPrime.classList.remove('hidden-entity');
+      // }
 
-//       burgerRect.forEach(function (item) {
-//         item.classList.add('general-header__burger-rect--scrolled');
-//       });
+      // burgerRect.forEach(function (item) {
+      //   item.classList.add('general-header__burger-rect--scrolled');
+      // });
 
 //     } else {
 //       generalHeader.classList.remove('general-header--scrolled');
